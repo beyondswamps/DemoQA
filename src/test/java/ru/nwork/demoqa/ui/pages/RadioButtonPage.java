c12cf1fb2f1cdee0ad366f099e22992aaf71acd2
OlegZabolotnykh <beyondswamps@gmail.com>
2025-10-25 16:15:05 +0300

    Radio-Button page

---
+++ src/test/java/ru/nwork/demoqa/ui/pages/RadioButtonPage.java
@@ -0,0 +1,15 @@
+package ru.nwork.demoqa.ui.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class RadioButtonPage {
+    SelenideElement yesRadio = $("#yesRadio");
+    SelenideElement impressiveRadio = $("#impressiveRadio");
+    SelenideElement noRadio = $("#noRadio");
+    SelenideElement status = $(".text-success");
+
+    
+
+}
